21a50949ffe20c78a0dff361d29c9d143297018d
d4nielj <d4niel.djm@gmail.com>
2021-10-14 15:32:00 -0500

    Add test for negative numbers and raise exception

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 class Solver
   def factorial(num)
+    return 0 if num == 1
+
+    raise ArgumentError, 'number must be 0 or positive' if num.negative?
+
     res = 1
     until num < 1
       res *= num
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,5 +4,7 @@ describe Solver do
   it 'it returns the correct factorial of a number' do
     expect(Solver.new.factorial(5)).to eql 120
     expect(Solver.new.factorial(4)).to eql 24
+    expect(Solver.new.factorial(1)).to eql 0
+    expect { Solver.new.factorial(-24) }.to raise_error(ArgumentError, 'number must be 0 or positive')
   end
 end
